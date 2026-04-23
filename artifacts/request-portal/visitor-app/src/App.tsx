6dd13858dbb7646dc780935777854b7e20637819
mym-dev-ui <saddam.saqqar@icloud.com>
2026-04-23 03:53:48 +0300

    Create main App component

---
+++ artifacts/request-portal/visitor-app/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import StepOne from './pages/StepOne';
+
+export default function App() {
+  return (
+    <div dir="rtl" style={{ direction: 'rtl' }}>
+      <StepOne />
+    </div>
+  );
+}
